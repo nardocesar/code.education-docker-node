1ff8f496a2fda6aba5e1ccad9a5d6c50501645d3
Carlos Nardo <c.cesar.nardo@gmail.com>
2021-08-12 23:02:38 -0300

    adds README

---
+++ README.md
@@ -0,0 +1,3 @@
+# Desafio 02 (Docker) - Node + MySQL + NGINX
+
+Para executar utilize `docker-compose up -d`
--- database/init.sql
+++
@@ -1,6 +0,0 @@
-GRANT ALL PRIVILEGES ON nodedb.* TO 'root'@'%';
-GRANT ALL PRIVILEGES ON nodedb.* TO 'root'@'localhost';
-
-USE nodedb;
-
-CREATE TABLE people(id int not null auto_increment, name (varchar(255), primary key(id)));
